bb1305a272b85694b076b167727d6e9616124bd7
hira-mushtaq1 <86572509+hira-mushtaq1@users.noreply.github.com>
2022-11-28 21:39:10 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+// import FirstLogin from './Screens/FirstLogin';
+import ForthScreen from './Screens/ForthScreeen';
+import ThirdLogin from './Screens/ThirdLogin';
+import TodoApp from './Screens/TodoApp';
+
+// import SecondLogin from './Screens/SecondLogin';
+
+
+function App() {
+  return (
+    <>
+      <View>
+        {/* <FirstLogin /> */}
+        <TodoApp />
+
+        {/* <ForthScreen /> */}
+        {/* <ThirdLogin /> */}
+      </View>
+    </>
+  );
+}
+
+export default App;
