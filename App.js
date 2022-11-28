d7f1ec561bc5957c007e1ba4d9d31da67fd2c4a5
hira-mushtaq1 <86572509+hira-mushtaq1@users.noreply.github.com>
2022-11-28 22:23:39 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,25 +1,99 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-// import FirstLogin from './Screens/FirstLogin';
-import ForthScreen from './Screens/ForthScreeen';
-import ThirdLogin from './Screens/ThirdLogin';
-import TodoApp from './Screens/TodoApp';
+import React, {useState} from 'react';
+import {Image, StatusBar} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
+import logo from './assets/logo.png';
+export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-// import SecondLogin from './Screens/SecondLogin';
-
-
-function App() {
   return (
-    <>
-      <View>
-        {/* <FirstLogin /> */}
-        <TodoApp />
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={logo}
+        style={{width: 50, height: 50}}
+      />
 
-        {/* <ForthScreen /> */}
-        {/* <ThirdLogin /> */}
+      <StatusBar style="auto" />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email."
+          placeholderTextColor="#003f5c"
+          onChangeText={email => setEmail(email)}
+        />
       </View>
-    </>
+
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password."
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={password => setPassword(password)}
+        />
+      </View>
+
+      <TouchableOpacity>
+        <Text style={styles.forgot_button}>Forgot Password?</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.logintxt}>LOGIN</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  inputView: {
+    backgroundColor: '#ffff',
+    borderRadius: 30,
+    width: '70%',
+    height: 45,
+    marginBottom: 20,
+
+    alignItems: 'center',
+  },
+
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+    color: 'black',
+  },
+
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+    color: 'black',
+  },
+
+  loginBtn: {
+    width: '50%',
+    borderRadius: 25,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+    backgroundColor: '#ffff',
+  },
+  logintxt: {
+    color: 'black',
+  },
+});
